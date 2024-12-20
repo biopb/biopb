73c8adab890f6eb7b0d1528adea8a7f651525183
Ji Yu <jyu@uchc.edu>
2024-12-20 11:52:05 -0500

    Protocol udpate
      * add python package definition
      * add an annotation field
      * add an generic image processing call
      * fix typo

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -4,7 +4,7 @@
 protoc -o gen/descriptor.pb --include_source_info biopb/*/*
 
 # generate python files
-python -m grpc_tools.protoc -I . --python_out=gen/python/ --grpc_python_out=gen/python/ biopb/*/*
+python -m grpc_tools.protoc -I . --python_out=gen/python/ --grpc_python_out=gen/python/ biopb/image/*
 
 # generate java files
 # protoc --plugin=protoc-gen-grpc-java --grpc-java_out=gen/java/ --proto_path=. biopb/*/*
